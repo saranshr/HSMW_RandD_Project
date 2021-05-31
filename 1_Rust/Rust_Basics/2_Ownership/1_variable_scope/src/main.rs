591de73600801631f9dc1b6ac7c7b19901e8fbbb
Saransh Rastogi <srastogi@hs-mittweida.de>
2021-05-31 23:41:02 +0200

    variable scope in Rust

---
+++ 1_Rust/Rust_Basics/2_Ownership/1_variable_scope/src/main.rs
@@ -0,0 +1,10 @@
+// This is similar to C and other programming languages
+
+fn main() {
+    // A scope is the range within a program for which an item is valid
+    let s = "hello";
+    /* 
+    -> The variable s refers to a string literal, where the value of the string is hardcoded into the text of our program
+    -> The variable is valid from the point at which its declared until the end of the current scope
+    */
+}                       // the scope is over here, so the variable is not valid after this scope. In this case it is the main function.
